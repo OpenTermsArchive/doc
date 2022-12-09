c56d06e8efb2de9642913ef0e765366ab0dbfbb4
clementbiron <clementbiron@users.noreply.github.com>
2022-12-09 11:32:33 +0000

    deploy: 29162f0e3365770f8dc2575e693f5aad97cd86b5

---
+++ js/languageSwitcher.js
@@ -0,0 +1,11 @@
+(() => {
+  // <stdin>
+  document.addEventListener("DOMContentLoaded", function() {
+    $languageSwitcher = document.querySelector(".languageSwitcher");
+    document.querySelectorAll(".languageSwitcher_current, .languageSwitcher_item").forEach(($el) => {
+      $el.addEventListener("click", function(e) {
+        $languageSwitcher.classList.toggle("languageSwitcher__isOpen");
+      });
+    });
+  });
+})();
