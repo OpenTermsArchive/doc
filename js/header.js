5fa16e17ad5b8bbfd67c773d3b331e61c1e57bbe
clementbiron <clementbiron@users.noreply.github.com>
2023-01-16 14:56:15 +0000

    deploy: 3fed33b3da77ce9ceeb789bb463412e70733dde5

--- js/header.js
+++ js/header.js
@@ -1,9 +1,9 @@
 (() => {
   // <stdin>
-  document.addEventListener("DOMContentLoaded", function() {
-    $header = document.querySelector(".header");
+  document.addEventListener("DOMContentLoaded", () => {
+    const $header = document.querySelector(".header");
     document.querySelectorAll(".header_openLink, .header_closeLink").forEach(($el) => {
-      $el.addEventListener("click", function(e) {
+      $el.addEventListener("click", () => {
         $header.classList.toggle("header__isOpen");
       });
     });
--- js/languageSwitcher.js
+++ js/languageSwitcher.js
@@ -1,9 +1,9 @@
 (() => {
   // <stdin>
-  document.addEventListener("DOMContentLoaded", function() {
-    $languageSwitcher = document.querySelector(".languageSwitcher");
+  document.addEventListener("DOMContentLoaded", () => {
+    const $languageSwitcher = document.querySelector(".languageSwitcher");
     document.querySelectorAll(".languageSwitcher_current, .languageSwitcher_item").forEach(($el) => {
-      $el.addEventListener("click", function(e) {
+      $el.addEventListener("click", () => {
         $languageSwitcher.classList.toggle("languageSwitcher__isOpen");
       });
     });
